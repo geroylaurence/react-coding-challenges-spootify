678d6b7451c19a08e90004e9af8fb27c5659efa9
geroylaurence <48232303+geroylaurence@users.noreply.github.com>
2021-03-04 22:43:18 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ export default {
   api: {
     baseUrl: 'https://api.spotify.com/v1',
     authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: 'c4a02ce4b5384e4e9497891d9946d021',
-    clientSecret: '4fdf3f10d2534b2cb0a472602ee8d8a6'
+    clientId: '',
+    clientSecret: ''
   },
   endpoints: {
     newReleased: 'new-releases',
